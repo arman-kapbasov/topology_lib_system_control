ef69ba7422f981d485ffa821709e0e12235aceec
arman-kapbasov <arman.kapbasov@hpe.com>
2016-04-04 14:04:56 -0700

    Updated lib files

---
+++ doc/index.rst
@@ -0,0 +1,49 @@
+.. toctree::
+   :hidden:
+
+   developer
+   topology_lib_system_fail/topology_lib_system_fail
+
+==============================================
+system_fail Communication Library for Topology
+==============================================
+
+.. container:: float-right
+
+   .. image:: _static/images/logo.png
+
+Check if switch has system services that failed
+
+
+Documentation
+=============
+
+- :doc:`Developer Guide. <developer>`
+- :doc:`Internal Documentation Reference. <topology_lib_system_fail/topology_lib_system_fail>`
+
+
+Development
+===========
+
+- `Project repository. <https://github.com/arman-kapbasov/topology_lib_system_fail.git>`_
+
+
+License
+=======
+
+::
+
+   Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing,
+   software distributed under the License is distributed on an
+   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+   KIND, either express or implied.  See the License for the
+   specific language governing permissions and limitations
+   under the License.
---
+++ setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    """
+    Read a file relative to setup.py location.
+    """
+    import os
+    here = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(here, filename)) as fd:
+        return fd.read()
+
+
+def find_version(filename):
+    """
+    Find package version in file.
+    """
+    import re
+    content = read(filename)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+def find_requirements(filename):
+    """
+    Find requirements in file.
+    """
+    import string
+    content = read(filename)
+    requirements = []
+    for line in content.splitlines():
+        line = line.strip()
+        if line and line[:1] in string.ascii_letters:
+            requirements.append(line)
+    return requirements
+
+
+setup(
+    name='topology_lib_system_fail',
+    version=find_version('lib/topology_lib_system_fail/__init__.py'),
+    package_dir={'': 'lib'},
+    packages=find_packages('lib'),
+
+    # Dependencies
+    install_requires=find_requirements('requirements.txt'),
+
+    # Metadata
+    author='Hewlett Packard Enterprise Development LP',
+    author_email='arman.kapbasov@hpe.com',
+    description=(
+        'Check if switch has system services that failed'
+    ),
+    long_description=read('README.rst'),
+    url='https://github.com/arman-kapbasov/topology_lib_system_fail.git/tree/master/doc',
+    keywords='topology_lib_system_fail',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # Entry points
+    entry_points={
+        'topology_library_10': [
+            'system_fail = topology_lib_system_fail.library'
+        ]
+    }
+)
